4a8e51dea33f451f6caeb4065d8f6d55c7700bfc
AbigailEckstein <abigail.e275@gmail.com>
2025-07-12 19:22:29 -0500

    working on all of the pages

---
+++ ourApp/app/(tabs)/index.jsx
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+const Home = () => {
+    return (
+        <SafeAreaView style={{flex:1}}>
+            <Text>Home</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Home;
+
+const styles = StyleSheet.create({})
---
+++ ourApp/app/(tabs)/profile.jsx
@@ -0,0 +1,10 @@
+import {Stylesheet, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native';
+
+export default function Profile() {
+    return (
+        <SafeAreaView>
+            <Title>Profile</Title>
+        </SafeAreaView>
+    )
+}
